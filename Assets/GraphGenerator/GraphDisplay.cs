4fe142f35d402be6828dfc85a746dedf72368320
Sarthak Singh <ss269@uw.edu>
2019-10-12 18:09:34 -0700

    Mesh renderer done

---
+++ Assets/GraphGenerator/GraphDisplay.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphDisplay : MonoBehaviour
+{
+    private MeshGenerator meshGenerator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshGenerator = new MeshGenerator(new Vector2(0.01f, 0.01f));
+
+        Func<float, float, float> cone = (x, z) =>
+        {
+            float a = 1;
+            float b = 1;
+            float c = 1;
+
+            return c * Mathf.Sqrt((Mathf.Pow(x, 2) / Mathf.Pow(a, 2)) + (Mathf.Pow(z, 2) / Mathf.Pow(b, 2)));
+        };
+
+        MeshData meshData = meshGenerator.GenerateMeshData(
+            cone,
+            new Vector2(-1, -1),
+            new Vector2(1, 1)
+        );
+
+        Mesh mesh = new Mesh();
+        mesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
+        mesh.vertices = meshData.vertices;
+        mesh.triangles = meshData.triangles;
+        mesh.uv = meshData.uvs;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/GraphGenerator/MeshGenerator/MeshGenerator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MeshData
+{
+    public Vector3[] vertices;
+    public Vector2[] uvs;
+    public int[] triangles;
+}
+
+public class MeshGenerator
+{
+    private Vector2 stepSize;
+
+    public MeshGenerator(Vector2 stepSize)
+    {
+        this.stepSize = stepSize;
+    }
+
+    public MeshData GenerateMeshData(Func<float, float, float> surface, Vector2 lowerBounds, Vector2 upperBounds)
+    {
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> triangles = new List<int>();
+
+        Vector2Int size = new Vector2Int(
+            Mathf.FloorToInt((upperBounds.x - lowerBounds.x) / stepSize.x),
+            Mathf.FloorToInt((upperBounds.y - lowerBounds.y) / stepSize.y)
+        );
+
+        Vector3[][] surfaceMap = new Vector3[size.x][];
+        for (int x = 0; x < size.x; x++)
+        {
+            surfaceMap[x] = new Vector3[size.y];
+            for (int z = 0; z < size.y; z++)
+            {
+                surfaceMap[x][z] = new Vector3(
+                    lowerBounds.x + (x * stepSize.x),
+                    surface(lowerBounds.x + (x * stepSize.x), lowerBounds.y + (z * stepSize.y)),
+                    lowerBounds.y + (z * stepSize.y)
+                );
+            }
+        }
+
+        for (int x = 0; x < (size.x - 1); x++)
+        {
+            for (int z = 0; z < (size.y - 1); z++)
+            {
+                // Triangle 1
+                vertices.Add(surfaceMap[x][z]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(0, 0));
+
+                vertices.Add(surfaceMap[x][z + 1]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(0, 1));
+
+                vertices.Add(surfaceMap[x + 1][z + 1]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(1, 1));
+
+                // Triangle 2
+                vertices.Add(surfaceMap[x][z]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(0, 0));
+
+                vertices.Add(surfaceMap[x + 1][z + 1]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(1, 1));
+
+                vertices.Add(surfaceMap[x + 1][z]);
+                triangles.Add(vertices.Count - 1);
+                uvs.Add(new Vector2(1, 0));
+            }
+        }
+
+        return new MeshData() {
+            vertices = vertices.ToArray(),
+            uvs = uvs.ToArray(),
+            triangles = triangles.ToArray()
+        };
+    }
+}
