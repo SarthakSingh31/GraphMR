0430bd3edde3594b01eb2f01e84539609624c15e
Sarthak Singh <ss269@uw.edu>
2019-10-12 20:57:38 -0700

    Added some stuff

---
+++ Assets/EquationEvaluator/TreeDataTypes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Expression
+{
+    public Expression left;
+    public Expression right;
+    public abstract float eval();
+}
+
+public abstract class Function : Expression
+{
+
+}
+
+public abstract class Operator : Expression
+{
+
+}
---
+++ Assets/ImageProcessor/ImageProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ImageProcessor
+{
+    private string api_url = "https://api.mathpix.com/v3/text";
+
+    public bool updated = false;
+    public string text;
+
+    public IEnumerator PostImage(string imageData)
+    {
+        UnityWebRequest request = new UnityWebRequest(api_url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes("{\"src\": \"" + imageData + "\"}");
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.SetRequestHeader("content-type", "application/json");
+        request.SetRequestHeader("app_id", "pmao_uw_edu");
+        request.SetRequestHeader("app_key", "619f6d7f1f034acafdeb");
+
+        request.downloadHandler = new DownloadHandlerBuffer();
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            MonoBehaviour.print(request.error);
+        }
+        else
+        {
+            updated = true;
+            text = request.downloadHandler.text;
+        }
+    }
+}
